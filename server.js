6ff2fa34adcec90029c2bcd4c58e375d8240893f
Kevin Simper <kevin.simper@gmail.com>
2016-09-07 01:07:22 +0200

    Get the containers from the API

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+import aws4 from 'hyper-aws4'
+import fetch from 'node-fetch'
+
+const signOption = {
+  method: 'GET',
+  credential: {
+    accessKey: process.env.HYPER_ACCESS,
+    secretKey: process.env.HYPER_SECRET
+  }
+}
+
+
+
+let url = 'https://us-west-1.hyper.sh/containers/json'
+const headers = aws4.sign(Object.assign({}, {url}, signOption))
+let containers = fetch(url, {method: signOption.method, headers}).then((res) => {
+    return res.json()
+}).then((c) => console.log(c))
