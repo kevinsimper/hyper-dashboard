f60ac92b1d21044ae277d1a3c03fc40ac34cdc64
Kevin Simper <kevin.simper@gmail.com>
2016-09-15 09:45:15 +0200

    Serve static files with express

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import Hyper from 'hyper-api'
 let api = new Hyper()
 var app = express()
 
+app.use(express.static('build'))
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
