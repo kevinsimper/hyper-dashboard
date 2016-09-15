0d1dcad7ed0777859d102c4e8971c9b22f47f55a
Kevin Simper <kevin.simper@gmail.com>
2016-09-15 12:23:42 +0200

    Show more information on each bubble

--- src/components/ContainerBoble/index.js
+++ src/components/ContainerBoble/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import './style.css'
 
 export default class ContainerBoble extends Component {
   render() {
     const { data } = this.props
     return (
-      <div className='Container'>{data.Command}</div>
+      <div className='Container'>
+        <div>{data.Image}</div>
+        <div>{data.Command}</div>
+        <div>{data.Status}</div>
+        <div><a href={'http://' + data.Labels['sh.hyper.fip']}>{data.Labels['sh.hyper.fip']}</a></div>
+      </div>
     )
   }
 }
