b07efe69feacca40c312b4ebe798a4a2e7b93f7f
Kevin Simper <kevin.simper@gmail.com>
2016-09-17 00:43:42 +0200

    Make a socketio integration for logs

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import Hyper from 'hyper-api'
+import socketIO from 'socket.io'
+import http from 'http'
+
+var app = express()
+let server = http.Server(app)
+let io = socketIO(server)
 
 let api = new Hyper()
-var app = express()
 
 app.use(express.static('build'))
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -16,6 +28,6 @@ app.get('/containers/json', (req, res) => {
   api.get('/containers/json').then(c => res.send(c)).catch(e => console.log(e))
 })
 
-app.listen(9000, () => {
+server.listen(9000, () => {
   console.log('Listening on port 9000')
 })
